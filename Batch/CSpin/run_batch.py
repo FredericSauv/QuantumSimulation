3a84e6fc963307ec1a3a229cb29327447548bfad
Fred Sauvage <frederic.sauv@gmail.com>
2018-04-30 12:20:05 +0100

    finalize the py file such that it can be called with args from the shell and display three behaviors

--- Batch/CSpin/run_batch.py
+++ Batch/CSpin/run_batch.py
@@ -11,28 +11,31 @@ from  QuantumSimulation.Simulation.Spin.ControlledSpinOptimBatch import Controll
 
 
 #==============================================================================
-# 3 BEHAVIORS DEPENDING ON THE FIRST PARAMETERS:
-#   + "gen_meta_configs" generate configfiles from a metaconfig file
+# 3 BEHAVIORS DEPENDING ON THE FIRST PARAMETER:
+#   + "gen_configs" generate configfiles from a metaconfig file
 #   + "run_one_config" run cspinoptim based on a config file
 #   + "run_meta_config" run cspinoptim based on a metaconfigfile
 #==============================================================================
-if(len(sys.argv) > 4):
+if(len(sys.argv) > 3):
     print("Wrong number of args", file = sys.stderr)
 else:
-	type_task = sys.argv[1]
-	file_input = sys.argv[2]
-    if(type_task == "gen_meta_configs"):
-    	# put them in Config/ XXX
-    	OptBatch.parse_and_save_meta_config(file_input)
+    type_task = sys.argv[1]
+    file_input = sys.argv[2]
+    if(type_task == "gen_configs"):
+        # put them in Config/ XXX
+        OptBatch.parse_and_save_meta_config(file_input, output_folder = 'Config')
 
-    else(type_task == "run_one_config"):
-		batch = OptBatch(file_input)
-		batch.run_procedures(saveFreq = 1, splitRes = True, printInfo = False)
+    elif(type_task == "run_one_config"):
+        batch = OptBatch(file_input)
+        batch.run_procedures(saveFreq = 1, splitRes = True, printInfo = False)
 
     elif(type_task == "run_meta_configs"):
-		batch = OptBatch.from_meta_config(file_input)
-		batch.run_procedures(saveFreq = 1, splitRes = True, printInfo = False, debug = True)
-
+        # Instantiate Batch with a metaconfig file
+        batch = OptBatch.from_meta_config(file_input)
+        batch.run_procedures(saveFreq = 1, splitRes = True, printInfo = False, debug = True)
+        
+    else:
+        print("first argument not recognized", file = sys.stderr)
 #res = OptBatch.read_res(folderName = 'Output/TestBatch', allPrefix ='')
 
 
