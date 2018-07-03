858112fef68afa13c897b00cb0e1d9023e030d57
Fred Sauvage <frederic.sauv@gmail.com>
2018-07-03 19:35:49 +0100

    New batches:small fourier/crab

---
+++ Simulation/BH1D/Batch/Input/LN5_pcw15.py
@@ -0,0 +1,14 @@
+model_dico {'L':5,'Nb':5,'mu':0,'T':4.889428431607287,'dt':0.01,'flag_intermediate':False,'setup':'1','state_init':'GS_i','state_tgt':'GS_inf','fom':['f2t2:neg_fluence:0.0001_smooth:0.005'],'fom_print':True,'track_learning':True,'ctl_shortcut':'owbds01_pwc15','_FLAG_NAME':'1T'} {'T':2.4447142158036437,'_FLAG_NAME':'05T'} {'T':9.778856863214575,'_FLAG_NAME':'2T'} {'T':14.668285294821862,'_FLAG_NAME':'3T'}
+
+testing_dico {'fom':['f2t2:neg_fluence:0.0001_smooth:0.005','f2t2','fluence','smooth','varN']}
+
+optim_dico {'algo':'NM','init_obj':'uniform_0_1','maxfev':None,'maxiter':None,'_FLAG_NAME':'NM01'} {'algo':'DE','popsize':3,'maxiter':None,'init_obj':None,'_FLAG_NAME':'DE3'} {'algo':'DE','popsize':5,'maxiter':None,'init_obj':None,'_FLAG_NAME':'DE5'} {'algo':'DE','popsize':10,'maxiter':None,'init_obj':None,'_FLAG_NAME':'DE10'} {'algo':'BO2','maxiter':500,'num_cores':1,'init_obj':100,'acq':'EI','_FLAG_NAME':'BO500EI'} {'algo':'BO2','maxiter':1000,'num_cores':1,'init_obj':100,'acq':'EI','_FLAG_NAME':'BO1000EI'} {'algo':'BO2','maxiter':500,'num_cores':1,'init_obj':100,'acq':'LCB','_FLAG_NAME':'BO500LCB'} {'algo':'BO2','maxiter':1000,'num_cores':1,'init_obj':100,'acq':'LCB','_FLAG_NAME':'BO1000LCB'}
+
+_RDM_RUNS 30
+_RDM_FIXSEED True
+_OUT_PREFIX 'res'
+_OUT_FOLDER 'Output/Test'
+_OUT_COUNTER 0
+_OUT_NAME None
+_OUT_STORE_CONFIG True
+_MP_FLAG False
