cd6fd5e0144434e44ecec1fefd5eb44157d22b74
Fred Sauvage <frederic.sauv@gmail.com>
2018-07-05 18:08:46 +0100

    opt_more is only added for BO2

--- Simulation/BH1D/learn_1DBH.py
+++ Simulation/BH1D/learn_1DBH.py
@@ -10,6 +10,7 @@ import sys
 import pdb
 import copy
 import numpy as np
+import matplotlib.pylab as plt
 from functools import partial
 if(__name__ == '__main__'):
     sys.path.append("../../../")
@@ -91,6 +92,65 @@ class learner1DBH(Batch.Batch):
             model_dico['control_obj'] = learner1DBH._build_control_from_string(control,
                       self.random_gen, model_dico)
 
+# ---------------------------
+#   COLLECTING RESULTS ## WORKAROUND BUGS
+# ---------------------------
+    @classmethod
+    def eval_from_onefile_bug(cls, name):
+        """ eval the first element of the first line of a file """
+        res = ut.eval_from_file_supercustom(name, evfunc = pFunc_base.eval_with_pFunc)
+        return res
+    
+    @classmethod
+    def collect_and_process_res_bug(cls, key_path = [], nameFile = None, allPrefix = 'res_', 
+                                folderName = None, printing = False, ideal_evol = False):
+        collect = learner1DBH.collect_res_bug(key_path, nameFile, allPrefix, folderName)
+        process = learner1DBH.process_collect_res(collect, printing, ideal_evol)
+        
+        return process
+    
+    @classmethod
+    def read_res_bug(cls, nameFile = None, allPrefix = 'res_', folderName = None):
+        """ Extract result(s) stored in a (several) txt file (s) and return them  
+        in a (list) of evaluated objects
+        Rules: 
+            +if nameFile is provided it will try to match it either in folderName
+             if provided or  in the current directory
+            +if no nameFile is provided it will try to match the allPrefix or 
+             fetch everything if None (directory considered follow the same rules 
+             based on folderName as before)
+        """
+        listFileName = ut.findFile(nameFile, allPrefix, folderName)
+        results = [learner1DBH.eval_from_onefile_bug(f) for f in listFileName]
+        #results = [ut.file_to_dico(f, evfunc = (lambda x: eval(x)) ) for f in listFileName]        
+        return results
+
+    @classmethod
+    def collect_res_bug(cls, key_path = [], nameFile = None, allPrefix = 'res_', folderName = None):
+        """Extract results stored in (a) txt file(s) and group them according to 
+        some key values (where key_path provides the path in the potentially 
+        nested structure of the results to find the key(s))
+        
+        Output:
+            a dictionary where key is the concatenation of the unique set of 
+            keys found and value is the res is a list of all res matching this key
+        """
+        listRes = cls.read_res_bug(nameFile, allPrefix, folderName)
+        res_keys = [tuple([ut.extract_from_nested(res, k) for k in key_path]) 
+                    for res in listRes]
+        res_keys_unique = list(set(res_keys))
+        res = {ut.concat2String(*k_u):[listRes[n] for n, r in enumerate(res_keys) 
+                if r == k_u] for k_u in res_keys_unique}
+        return res
+
+
+
+# ---------------------------
+#   END BUG SECTION
+# ---------------------------
+
+    def extractFromDico(dico, listRes = [], listParams = [], dicoConstraints = {}):
+        raise NotImplementedError()
             
     @classmethod
     def _build_control_from_string(cls, control, random_gen=None, context_dico = None):
@@ -306,7 +366,7 @@ class learner1DBH(Batch.Batch):
     
     
     @classmethod
-    def ideal_learning(run):
+    def ideal_learning(cls, run):
         """ Compute the fom (under testing conditions) for the different functions 
         found along optimization and also the distance between parameters
         """
@@ -324,7 +384,7 @@ class learner1DBH(Batch.Batch):
         return names_fom, res, res_params_dist
         
     @classmethod
-    def ideal_learning_from_list_res(list_res):
+    def ideal_learning_from_list_res(cls, list_res):
         pdb.set_trace()
         tmp = [learner1DBH.ideal_learning(res) for res in list_res]
         nb_run = len(list_res)
@@ -332,13 +392,99 @@ class learner1DBH(Batch.Batch):
         res = [[np.array([tmp[r][:,0], tmp[r][:,1+n]]) for r in range(nb_run)] for n in range(nb_fom)]
         return res
 
+
     @classmethod
-    def runSimul(dico_simul, params):
+    def rebuild_one_res(cls, res):
+        pass
+
+    @classmethod
+    def runSimul(cls, dico_simul, params):
         """ from a dico containing the parameters of the simulations and a control 
             function get the results of the simulation"""
         model_tmp = bh1d.BH1D(**dico_simul)
         res = model_tmp(params)
         return res
+
+    @classmethod
+    def find_best_run_in_dico_simul(cls, dico_simul, path_criterion, test = 'min', filt = None):
+        """ dico_simul = {'name_simul':list_runs}
+            list_runs = [run1, ..., run30]
+        """
+        best = np.inf
+        index_best =[None, None]
+        if(test == 'max'):
+            mult = -1
+        else:
+            mult = 1
+        if(filt is None):
+            filt = lambda x:True
+        
+        for k, res in dico_simul.items():
+            if(filt(k)): 
+                for n, run in enumerate(res):
+                    tmp = mult * ut.extract_from_nested(run, path_criterion)
+                    if tmp < best:
+                        best = tmp 
+                        index_best[0] = k
+                        index_best[1] = n
+
+        if(test == 'max'):
+            best *= -1
+        print(best)
+        return copy.copy(dico_simul[index_best[0]][index_best[1]])
+
+    @classmethod     
+    def study_convergence_one_res(cls, res):
+        path_X = ['test_more', 'X_evol']
+        path_Y = ['test_more', 'Y_evol']
+        path_nbinit = ['config', 'optim_dico', 'init_obj']
+
+        try:
+            X = ut.extract_from_nested(res, path_X)
+            Y = ut.extract_from_nested(res, path_Y)
+            nbinit = ut.extract_from_nested(res, path_nbinit)
+            study_convergence(X, Y, beg = nbinit, end = 15)
+        except:
+            print("couldn't build the graph probably data missing")
+        
+        
+    
+def dist(x, y):
+    d = np.squeeze(x)-np.squeeze(y)
+    return np.dot(d, d)
+        
+def get_dist_successive(X, n_ev = None):
+    distance = [dist(x_n, X[n-1]) for n, x_n in enumerate(X[1:])]
+    
+    if(n_ev is None):
+        n_ev = np.arange(1, len(X)+1)
+    return [np.array([0]+n_ev), np.array([0]+distance)]
+    
+def get_best_so_far(Y, n_ev=None):
+    best_tmp = np.Inf
+    n_best = []
+    y_best = []
+    if(n_ev is None):
+        n_ev = np.arange(1, len(Y)+1)
+    for n, y_n in enumerate(Y):
+        if (y_n < best_tmp):
+            best_tmp = y_n
+            n_best.append(n_ev[n])
+            y_best.append(y_n)
+    return [np.array(n_best), np.array(y_best)]
+
+def study_convergence(X, Y, end = 0, beg = 0):
+    if(len(X) != len(Y)):
+        SystemError("X and Y should have the same length")
+    nb_obs = len(X)
+    nev, dist = get_dist_successive(X)
+    nevbest, Ybest = get_best_so_far(Y)
+    distbest = dist[np.array([n in nevbest for n in nev])]
+    fig, ax = plt.subplots()
+    plt.plot(nev, dist, 'b')
+    plt.scatter(nevbest, distbest, color = 'b')
+    ax.axvspan(0, beg, alpha=0.5, color='grey')
+    ax.axvspan(nb_obs-end, nb_obs, alpha=0.5, color='green')
 
 #==============================================================================
 #                   TESTING
