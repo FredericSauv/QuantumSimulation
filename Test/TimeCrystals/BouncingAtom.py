f5cfb67cfef13ba5ec8c61586703ff670c38565f
FS <Frederic.sauv@gmail.com>
2018-10-17 06:47:42 +0100

    add height after a certain number of periods

--- Test/TimeCrystals/BouncingAtom.py
+++ Test/TimeCrystals/BouncingAtom.py
@@ -8,6 +8,8 @@ Created on Thu Oct  4 19:58:08 2018
 from scipy.integrate import ode
 import numpy as np
 import matplotlib.pylab as plt
+from matplotlib import rc
+rc('text', usetex=True)
 import functools
 
 class model:
@@ -156,7 +158,6 @@ class model:
 
 if __name__ == '__main__':
     mod = model()
-
     #Arek's Mathematica
     mod.scan_h(delta = 1, href = 1/2 * (3*np.pi)**(2/3), omega = np.pi**(2/3)/(3**(1/3)), s=1.0)
 
@@ -164,24 +165,35 @@ if __name__ == '__main__':
     # Look at the impact of changing omega when there is nois ein the initial
     # height (h_ref). Results are obtained for s = 40 (still it doesn't seem
     # that changing it has impact)
-    h_ref = 10.0
+    h_ref = 328.85
     s = 40
+    print(mod.get_omega_perfect(328.85, 40))
+    periods = [1, 6]
     omega_range = np.linspace(0.8, 1.2, 200)
-    res_noise_10trials = np.zeros(len(omega_range))
-    res_noise_100trials = np.zeros(len(omega_range))
-    res_no_noise = np.zeros(len(omega_range))
+    res_noise_10trials = np.zeros((len(omega_range), len(periods)))
+    res_noise_100trials = np.zeros((len(omega_range), len(periods)))
+    res_no_noise = np.zeros((len(omega_range), len(periods)))
 
     omega_ref = s * np.pi/np.sqrt(2 * h_ref)
     for n_om, om in enumerate(omega_ref * omega_range):
-        res_noise_10trials[n_om] = mod.simulate_noisy_h0(om, h_ref, noise_h0 = 0.2, nb_repeat = 10, s = s, nb_period = [1, 4])
-        res_noise_100trials[n_om] = mod.simulate_noisy_h0(om, h_ref, noise_h0 = 0.2, nb_repeat = 100, s = s, nb_period = 1)
-        res_no_noise[n_om] = mod.simulate_noisy_h0(om, h_ref, noise_h0 = 0, nb_repeat = 1, s = s, nb_period = 1)
+        res_noise_10trials[n_om, :] = mod.simulate_noisy_h0(om, h_ref, noise_h0 = 0.2, nb_repeat = 10, s = s, nb_period = periods)
+        res_noise_100trials[n_om, :] = mod.simulate_noisy_h0(om, h_ref, noise_h0 = 0.2, nb_repeat = 100, s = s, nb_period = periods)
+        res_no_noise[n_om, :] = mod.simulate_noisy_h0(om, h_ref, noise_h0 = 0, nb_repeat = 1, s = s, nb_period = periods)
     
-    plt.plot(omega_range,res_noise_10trials, label = 'noise_20pc_10trials')
-    plt.plot(omega_range,res_noise_100trials, label = 'noise_20pc_100trials')
-    plt.plot(omega_range,res_no_noise, label = 'no noise')
+    plt.plot(omega_range,res_noise_10trials[:,0]/h_ref, label = 'noise:0.2, 10trials')
+    plt.plot(omega_range,res_noise_100trials[:,0]/h_ref, label = 'noise:0.2, 100trials')
+    plt.plot(omega_range,res_no_noise[:,0]/h_ref, label = 'no noise')
     plt.legend()
-    plt.xlabel('omega / omega_ref')
-    plt.ylabel('|h(T)-h_ref|')
-    plt.savefig('noise_0.2_h0_vary_omega.pdf')
+    plt.legend(fontsize = 12)
+    plt.xlabel(r"$\omega / \omega_{ref}$", size = 18)
+    plt.ylabel(r"$|h(T)-h_{ref}|/ h_{ref}$", size = 18)
+    plt.savefig('noise_0_2_h0_329_lambda_0_01.pdf')
 
+
+    plt.plot(omega_range,res_noise_10trials[:,1]/h_ref, label = 'noise:0.2, 10trials')
+    plt.plot(omega_range,res_noise_100trials[:,1]/h_ref, label = 'noise:0.2, 100trials')
+    plt.plot(omega_range,res_no_noise[:,1]/h_ref, label = 'no noise')
+    plt.legend(fontsize = 12)
+    plt.xlabel(r"$\omega / \omega_{ref}$", size = 18)
+    plt.ylabel(r"$|h(6T)-h_{ref}|/ h_{ref}$", size = 18)
+    plt.savefig('noise_0_2_h0_329_lambda_0_01_6T.pdf',bbox_inches='tight')
