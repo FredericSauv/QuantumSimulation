ecb1d560ccf934b348bb57e953dc93036f86504a
Fred Sauvage <frederic.sauv@gmail.com>
2019-01-11 20:12:46 +0000

    test on multioutput

---
+++ Test/GPy/coreg/coregionalization.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 10 11:43:25 2019
+https://github.com/SheffieldML/notebook/blob/master/GPy/coregionalized_regression_tutorial.ipynb
+@author: fred
+"""
+
