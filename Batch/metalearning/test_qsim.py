e1517bcab85e85c05aa1400142814872b596b1a6
Fred Sauvage <frederic.sauv@gmail.com>
2019-11-29 13:08:05 +0000

    more testing

--- Batch/metalearning/test_qsim.py
+++ Batch/metalearning/test_qsim.py
@@ -29,21 +29,29 @@ y = control(x)
 plt.plot(x, y)
 
 
-# Test BO
-alpha = np.random.normal(0,1)
-
+# Test simulator
+seed = None
+np.random.seed(seed)
+alpha = 0 #np.random.normal(0,1)
 s = sim.Simulator(DICT_CONTROL, DICT_MODEL)
 
+# Test BO
 bounds = [{'name': str(i), 'type': 'continuous', 'domain': (0, 1)} for i in range(s.nb_params)]
 DICT_BO = {'domain': bounds, 'acquisition_type': 'LCB', # Selects the Expected improvement
          'initial_design_numdata': 25, 'acquisition_weight':3,'maximize':True}
 myBopt = GPyOpt.methods.BayesianOptimization(f=s, **DICT_BO)
 myBopt.run_optimization(250)   
 
+# what is stored
 dict_res = {'X':np.array(myBopt.X),'Y':np.array(myBopt.Y), 'x_opt':myBopt.x_opt, 
-            'fx_opt':myBopt.fx_opt}
+            'fx_opt':myBopt.fx_opt, 'SEED':seed, 'call_f':s._f_calls,'alpha':s.control_fun.alpha}
 
 
-# alpha = 1 good params
+# for alpha = 0 good params
 # x_opt = np.array([0.72887749, 0.79911432, 0.82320549, 0.95501789, 0.88583126])
 # fx_opt = -0.9622391074818919
+
+# test reading results
+res0 = sim.read_results('_res/res/alpha_std1/alpha_std1/res0.txt')
+print(res0['alpha'])
+
